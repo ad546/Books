7a5157a494aa48727059fad6ae089427a2b90cc1
ad546 <ad546github@gmail.com>
2018-08-17 12:16:20 -0500

    template complete

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -5,11 +5,46 @@ class ListBooks extends React.Component {
 
     render () {
         return (
-            <div>
-                <Link   
-                    to='/search'
-                    className='open-search'
-                >Add Book</Link>
+            <div className="app">
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Currently Reading</h2>
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {/* list books */}
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Want to Read</h2>
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {/* list books */}
+                                    </ol>
+                                </div>
+                            </div>
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Read</h2>
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {/* list books */}
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Link   
+                        to='/search'
+                        className='open-search'
+                    >Add Book</Link>
+                </div>
             </div>
         )
     }
